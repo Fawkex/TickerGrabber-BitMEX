786036b7a6b5ee29836a2e746538be401c5b73b6
Fawkes Pan <ChinaBrowser@users.noreply.github.com>
2017-12-01 00:40:48 +0800

    Fix problem on writing data to redis.

--- grabber.py
+++ grabber.py
@@ -27,6 +27,7 @@ except ImportError:
     pass
 
 SYMBOL = 'XBTUSD'
+vals = {}
 
 config = {}
 config = apiconfig.get_config()
@@ -71,13 +72,14 @@ def WriteREDIS(ws,message):
             price = tickers['price']
             size = tickers['homeNotional']
             value = tickers['foreignNotional']
-            p.hset('timestamp:'+date,'symbol',SYMBOL)
-            p.hset('timestamp:'+date,'side',side)
-            p.hset('timestamp:'+date,'price',price)
-            p.hset('timestamp:'+date,'size',size)
-            p.hset('timestamp:'+date,'value',value)
-            p.hset('timestamp:'+date,'change',change)
-
+            vals['symbol'] = SYMBOL
+            vals['side'] = side
+            vals['price'] = price
+            vals['size'] = size
+            vals['value'] = value
+            vals['change'] = change
+            p.hmset("timestamp:"+date,vals)
+        
         p.execute()
             
     if 'pong' in message:
